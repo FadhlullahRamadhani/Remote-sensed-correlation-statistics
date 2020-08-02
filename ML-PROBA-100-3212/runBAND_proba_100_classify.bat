15a1612e500eefbf7039a62b975027ddcecce80c
danicoolmassey <danicoolmassey@users.noreply.github.com>
2020-08-03 10:27:19 +1200

    delete1
    delete

--- ML-PROBA-100-3212/runBAND_proba_100_classify.bat
+++
@@ -1,4 +0,0 @@
-Start C:\"Program Files"\R\R-3.5.0\bin\Rscript.exe C:\MasseyOffice1\Research\R-Script2019\Run-ML-LS8_S2_S1v1-step1-2019_classify_PROBA_3212_v2.R 4 0
-Start C:\"Program Files"\R\R-3.5.0\bin\Rscript.exe  C:\MasseyOffice1\Research\R-Script2019\Run-ML-LS8_S2_S1v1-step1-2019_classify_PROBA_3212_v2.R 4 1
-Start C:\"Program Files"\R\R-3.5.0\bin\Rscript.exe  C:\MasseyOffice1\Research\R-Script2019\Run-ML-LS8_S2_S1v1-step1-2019_classify_PROBA_3212_v2.R 4 2
-Start C:\"Program Files"\R\R-3.5.0\bin\Rscript.exe  C:\MasseyOffice1\Research\R-Script2019\Run-ML-LS8_S2_S1v1-step1-2019_classify_PROBA_3212_v2.R 4 3
