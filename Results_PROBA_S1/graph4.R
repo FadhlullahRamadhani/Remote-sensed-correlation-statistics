3b1641f533b108eb44005f0f10aea3b9a0b96eb5
danicoolmassey <danicoolmassey@users.noreply.github.com>
2021-01-12 13:27:19 +1300

    update graph1

--- Results_PROBA_S1/graph4.R
+++ Results_PROBA_S1/graph4.R
@@ -20,6 +20,8 @@ charts.data$tgl <- as.Date(substr(charts.data$period,2,20),"%Y-%m-%d")
 charts.data$tanggal <- format(charts.data$tgl,"%Y-%m-%d")
 charts.data$kabkec <- paste0(charts.data$KABUPATEN,"/ ",charts.data$KECAMATAN)
 charts.data$kabkec2 <- paste0(charts.data$KABUPATEN,"-",charts.data$KECAMATAN)
+charts.data$keckab <- paste0(charts.data$KECAMATAN,"/ ",charts.data$KABUPATEN)
+charts.data$keckab2 <- paste0(charts.data$KECAMATAN,"-",charts.data$KABUPATEN)
 
 fill <- c("#40b8d0", "#b2d183")
 
@@ -73,7 +75,9 @@ p1 <- ggplot(data = charts.data.regency.plot1,
 ggsave(filename="RS_regency1.png", plot=p1, device="png",
        height=7, width=7, units="in", dpi=600)
 
-cols_Phase2 <- c("Paddy_planting" = "#eb3528", "VEG" = "#66cc00","Paddy_harvested" = "#e128eb", "RIPE" = "#ffcc00")
+cols_Phase2 <- c("Paddy_planting" = "#66cc00", "VEG" = "#66cc00","Paddy_harvested" = "#ffcc00", "RIPE" = "#ffcc00")
+cols_Phase2_line <- c("Paddy_planting" = 1, "VEG" = 4,"Paddy_harvested" = 1, "RIPE" = 4)
+cols_Phase2_point <- c("Paddy_planting" = 16, "VEG" = 16,"Paddy_harvested" = 17, "RIPE" = 17)
 
 charts.data.regency2 <-charts.data %>% 
   select(KABUPATEN, tanggal, BL, VEG, RIPE, Paddy_planting, Paddy_harvested) %>%
@@ -84,15 +88,19 @@ charts.data.regency.plot2 <- melt(charts.data.regency2)
 charts.data.regency.plot2$date <- as.Date(charts.data.regency.plot1$tanggal)
 
 p2 <- ggplot(data = charts.data.regency.plot2, 
-             mapping = aes(x = date, y = value, color = variable)) +
-  geom_line(size=0.75) +
+             mapping = aes(x = date, y = value, color = variable, linetype = variable, shape= variable)) +
+  geom_line(size=1) +
+  geom_point() +
   xlab("Date") +
   ylab("Area (ha)") + 
   scale_y_continuous(labels = scales::comma) +
   scale_x_date(labels = date_format("%m-%Y"),date_breaks = "1 month") +
   facet_wrap(vars(KABUPATEN),  ncol=1) +
   theme_bw() + 
-  scale_colour_manual(name="Legend", values = cols_Phase2,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting","Vegetative","Paddy harvested","Ripening")) +
+  scale_shape_manual(name="Legend", values = cols_Phase2_point,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+  scale_linetype_manual(name="Legend", values = cols_Phase2_line,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+  scale_colour_manual(name="Legend", values = cols_Phase2,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+  guides(col = guide_legend(nrow = 2, byrow = TRUE))+
   theme(legend.position="bottom", legend.direction="horizontal",
         legend.title = element_blank(),axis.text.x = element_text(angle = 90, hjust = 1)) +
   theme(axis.text.x = element_text(angle = 30, hjust = 1), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -102,7 +110,7 @@ p2 <- ggplot(data = charts.data.regency.plot2,
 p2 
 
 ggsave(filename="RS_regency2.png", plot=p2, device="png",
-       height=7, width=7, units="in", dpi=600)
+       height=8, width=10, units="in", dpi=600)
 
 
 charts.data_SD <- filter(charts.data, area_RS > 5000)
@@ -139,9 +147,11 @@ ggsave(filename="RS_SD1.png", plot=p3, device="png",
        height=12, width=8, units="in", dpi=600)
 
 
-cols_Phase2 <- c("Paddy_planting" = "#eb3528", "VEG" = "#66cc00","Paddy_harvested" = "#e128eb", "RIPE" = "#ffcc00")
+cols_Phase2 <- c("Paddy_planting" = "#66cc00", "VEG" = "#66cc00","Paddy_harvested" = "#ffcc00", "RIPE" = "#ffcc00")
+cols_Phase2_line <- c("Paddy_planting" = 1, "VEG" = 4,"Paddy_harvested" = 1, "RIPE" = 4)
+cols_Phase2_point <- c("Paddy_planting" = 16, "VEG" = 16,"Paddy_harvested" = 17, "RIPE" = 17)
 
-charts.data_SD <- filter(charts.data, area_RS > 5000)
+charts.data_SD <- filter(charts.data, area_RS > 7000)
 charts.data.regency4 <-charts.data_SD %>% 
   select(kabkec, tanggal, VEG, RIPE, Paddy_planting, Paddy_harvested) %>%
   group_by(kabkec, tanggal) %>%
@@ -151,25 +161,30 @@ charts.data.regency.plot4 <- melt(charts.data.regency4)
 charts.data.regency.plot4$date <- as.Date(charts.data.regency.plot4$tanggal)
 
 p4 <- ggplot(data = charts.data.regency.plot4, 
-             mapping = aes(x = date, y = value, color = variable)) +
-  geom_line(size=0.75) +
+             mapping = aes(x = date, y = value, color = variable, linetype = variable)) +
+  geom_line(size=0.7) +
+  geom_point() +
   xlab("Date") +
   ylab("Area (ha)") + 
   scale_y_continuous(labels = scales::comma) +
-  scale_x_date(labels = date_format("%m-%Y"),date_breaks = "1 month") +
+  scale_x_date(labels = date_format("%m-%Y"),date_breaks = "2 month") +
   facet_wrap(vars(kabkec),  ncol=3) +
   theme_bw() + 
-  scale_colour_manual(name="Legend", values = cols_Phase2,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting","Vegetative","Paddy harvested","Ripening")) +
+  scale_shape_manual(name="Legend", values = cols_Phase2_point,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+  scale_linetype_manual(name="Legend", values = cols_Phase2_line,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+  scale_colour_manual(name="Legend", values = cols_Phase2,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+  guides(col = guide_legend(nrow = 2, byrow = TRUE))+
   theme(legend.position="bottom", legend.direction="horizontal",
         legend.title = element_blank(),axis.text.x = element_text(angle = 90, hjust = 1)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  theme(axis.text.x = element_text(angle = 30, hjust = 1), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank() ,legend.position = "bottom",legend.box.margin=margin(c(3,3,3,3)),
         axis.title.x = element_text(margin = unit(c(3, 0, 0, 0), "mm")))    
+
 
 p4 
 
 ggsave(filename="RS_SD2.png", plot=p4, device="png",
-       height=12, width=8, units="in", dpi=600)
+       height=8, width=12, units="in", dpi=600)
 
 listkabkec <- unique(charts.data$kabkec)
 listkabkec2 <- unique(charts.data$kabkec2)
@@ -180,14 +195,14 @@ for (i in 1:length(listkabkec)) {
   kabkec2_current <- listkabkec2[i]
   charts.data_SD <- filter(charts.data, kabkec2 == kabkec2_current)
   print(kabkec2_current)
-  charts.data_SD_plot1 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"VEGETATIVE-RIPENING","VEG",charts.data_SD$VEG)
-  charts.data_SD_plot2 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"VEGETATIVE-RIPENING","RIPE",charts.data_SD$RIPE)
-  charts.data_SD_plot3 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"RIPENING-HARVESTED","RIPE",charts.data_SD$RIPE)
-  charts.data_SD_plot4 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"RIPENING-HARVESTED","Paddy_harvested",charts.data_SD$Paddy_harvested)
-  charts.data_SD_plot5 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"PLANTING-HARVESTED","Paddy_planting",charts.data_SD$Paddy_planting)
-  charts.data_SD_plot6 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"PLANTING-HARVESTED","Paddy_harvested",charts.data_SD$Paddy_harvested)
-  charts.data_SD_plot7 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"VEGETATIVE-PLANTING","VEG",charts.data_SD$VEG)
-  charts.data_SD_plot8 <- cbind(charts.data_SD$kabkec,charts.data_SD$tanggal,"VEGETATIVE-PLANTING","Paddy_planting",charts.data_SD$Paddy_planting)
+  charts.data_SD_plot1 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Vegetative-Ripening (Remote sensing)","VEG",charts.data_SD$VEG)
+  charts.data_SD_plot2 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Vegetative-Ripening (Remote sensing)","RIPE",charts.data_SD$RIPE)
+  charts.data_SD_plot3 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Ripening (Remote sensing)-Harvested (Local statistics)","RIPE",charts.data_SD$RIPE)
+  charts.data_SD_plot4 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Ripening (Remote sensing)-Harvested (Local statistics)","Paddy_harvested",charts.data_SD$Paddy_harvested)
+  charts.data_SD_plot5 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Planting-Harvested (Local statistics)","Paddy_planting",charts.data_SD$Paddy_planting)
+  charts.data_SD_plot6 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Planting-Harvested (Local statistics)","Paddy_harvested",charts.data_SD$Paddy_harvested)
+  charts.data_SD_plot7 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Vegetative (Remote sensing)-Planting","VEG",charts.data_SD$VEG)
+  charts.data_SD_plot8 <- cbind(charts.data_SD$keckab,charts.data_SD$tanggal,"Vegetative (Remote sensing)-Planting","Paddy_planting",charts.data_SD$Paddy_planting)
   
   charts.data_SD_plot <- as.data.frame(rbind(charts.data_SD_plot1,charts.data_SD_plot2,charts.data_SD_plot3,charts.data_SD_plot4,
                                charts.data_SD_plot5,charts.data_SD_plot6,charts.data_SD_plot7,charts.data_SD_plot8),stringsAsFactors = FALSE) 
@@ -200,8 +215,9 @@ for (i in 1:length(listkabkec)) {
   charts.data_SD_plot$value <- as.numeric(charts.data_SD_plot$value)
 
   p5 <- ggplot(data = charts.data_SD_plot, 
-               mapping = aes(x = date, y = value, color = variable)) +
+               mapping = aes(x = date, y = value, color = variable, linetype = variable)) +
     geom_line(size=0.75) +
+    geom_point() +
     xlab("Date") +
     ylab("Area (ha)") +
     ggtitle(paste0("Comparison of remote-sensed data with local statistics on ",str_to_title(kabkec_current))) +
@@ -209,7 +225,9 @@ for (i in 1:length(listkabkec)) {
     scale_x_date(labels = date_format("%m-%Y"),date_breaks = "1 month") +
     facet_wrap(vars(typedata),  ncol=2) +
     theme_bw() + 
-    scale_colour_manual(name="Legend", values = cols_Phase2,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting","Vegetative","Paddy harvested","Ripening")) +
+    scale_shape_manual(name="Legend", values = cols_Phase2_point,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+    scale_linetype_manual(name="Legend", values = cols_Phase2_line,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
+    scale_colour_manual(name="Legend", values = cols_Phase2,breaks=c("Paddy_planting","VEG","Paddy_harvested","RIPE"),labels = c("Paddy planting area (Local statistics)","Vegetative area (Remote sensing)","Paddy harvested  area (Local statistics)","Ripening (Remote sensing)")) +
     theme(legend.position="bottom", legend.direction="horizontal",
           legend.title = element_blank(),axis.text.x = element_text(angle = 90, hjust = 1)) +
     theme(plot.title = element_text(hjust = 0.5),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -219,7 +237,7 @@ for (i in 1:length(listkabkec)) {
   #p5 
   
   ggsave(filename=paste0("sd/RS_SD_",kabkec2_current,".png"), plot=p5, device="png",
-         height=6, width=8, units="in", dpi=600)
+         height=8, width=12, units="in", dpi=600)
 
 }
 
